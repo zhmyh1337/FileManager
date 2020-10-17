bb5e936ba775a4a7af4dc7da75881052df153850
zhmyh1337 <70749157+zhmyh1337@users.noreply.github.com>
2020-10-17 20:13:57 +0300

    Changed ArgParser to cross-platform.

--- FileManager/FileManager.cs
+++ FileManager/FileManager.cs
@@ -10,10 +10,6 @@ namespace FileManager
         static void Main(string[] args)
         {
             
-//             CommandLine.Parser.Default.ParseArguments<Options>(ArgsParser.CommandLineToArgs(@"-r ""a b"" c d"))
-            CommandLine.Parser.Default.ParseArguments<Options>(args)
-              .WithParsed(RunOptions)
-              .WithNotParsed(HandleParseError);
         }
     }
 }
--- FileManager/src/Utilities/ArgsParser.cs
+++ FileManager/src/Utilities/ArgsParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Linq;
 using System.Text;
 
 namespace FileManager
@@ -8,34 +9,51 @@ namespace FileManager
     /// <summary>
     /// This static class parses string as args.
     /// See <see href="https://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp">here</see>.
+    /// If we weren't cross-platform, I'd use CommandLineToArgvW() API (see <see href="https://stackoverflow.com/a/749653">here</see>).
     /// </summary>
     static class ArgsParser
     {
-        [DllImport("shell32.dll", SetLastError = true)]
-        static extern IntPtr CommandLineToArgvW(
-        [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
-
-        public static string[] CommandLineToArgs(string commandLine)
+        public static IEnumerable<string> SplitCommandLine(string commandLine)
         {
-            int argc;
-            var argv = CommandLineToArgvW(commandLine, out argc);
-            if (argv == IntPtr.Zero)
-                throw new System.ComponentModel.Win32Exception();
-            try
-            {
-                var args = new string[argc];
-                for (var i = 0; i < args.Length; i++)
-                {
-                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
-                    args[i] = Marshal.PtrToStringUni(p);
-                }
+            bool inQuotes = false;
+            bool isEscaping = false;
 
-                return args;
-            }
-            finally
+            return commandLine.Split(c => {
+                if (c == '\\' && !isEscaping) { isEscaping = true; return false; }
+
+                if (c == '\"' && !isEscaping)
+                    inQuotes = !inQuotes;
+
+                isEscaping = false;
+
+                return !inQuotes && Char.IsWhiteSpace(c)/*c == ' '*/;
+            })
+            .Select(arg => arg.Trim().TrimMatchingQuotes('\"').Replace("\\\"", "\""))
+            .Where(arg => !string.IsNullOrEmpty(arg));
+        }
+
+        private static IEnumerable<string> Split(this string str, Func<char, bool> controller)
+        {
+            int nextPiece = 0;
+
+            for (int c = 0; c < str.Length; c++)
             {
-                Marshal.FreeHGlobal(argv);
+                if (controller(str[c]))
+                {
+                    yield return str.Substring(nextPiece, c - nextPiece);
+                    nextPiece = c + 1;
+                }
             }
+
+            yield return str.Substring(nextPiece);
+        }
+
+        private static string TrimMatchingQuotes(this string input, char quote)
+        {
+            if ((input.Length >= 2) && (input[0] == quote) && (input[input.Length - 1] == quote))
+                return input.Substring(1, input.Length - 2);
+
+            return input;
         }
     }
 }
