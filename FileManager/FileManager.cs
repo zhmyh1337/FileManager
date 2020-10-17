d2c2e80d42f77ffa7ca69966e829317cacc23808
zhmyh1337 <70749157+zhmyh1337@users.noreply.github.com>
2020-10-17 18:58:11 +0300

    Added NuGet CommandLineParser dependency. Added ArgsParser class. Renamed Program.cs->FileManager.cs. Changed package metadata.

---
+++ FileManager/FileManager.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FileManager
+{
+    class FileManager
+    {
+        static void Main(string[] args)
+        {
+            
+//             CommandLine.Parser.Default.ParseArguments<Options>(ArgsParser.CommandLineToArgs(@"-r ""a b"" c d"))
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+              .WithParsed(RunOptions)
+              .WithNotParsed(HandleParseError);
+        }
+    }
+}
---
+++ FileManager/src/Utilities/ArgsParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace FileManager
+{
+    /// <summary>
+    /// This static class parses string as args.
+    /// See <see href="https://stackoverflow.com/questions/298830/split-string-containing-command-line-parameters-into-string-in-c-sharp">here</see>.
+    /// </summary>
+    static class ArgsParser
+    {
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW(
+        [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        public static string[] CommandLineToArgs(string commandLine)
+        {
+            int argc;
+            var argv = CommandLineToArgvW(commandLine, out argc);
+            if (argv == IntPtr.Zero)
+                throw new System.ComponentModel.Win32Exception();
+            try
+            {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
+    }
+}
